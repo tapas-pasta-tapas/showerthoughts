16c7dba566cfe6dbdfdf240e4976e4c253c48434
leezhengjing <zhengjing.lee86@gmail.com>
2024-08-05 16:54:30 +0800

    Create home page and side bar

--- src/app/components/SideNavbar.tsx
+++ src/app/components/SideNavbar.tsx
@@ -4,7 +4,42 @@ import React from "react";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { ROOT, PROTECTED } from "../../lib/routes";
+import {
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+  BookIcon,
+  Book,
+  Calendar,
+} from "lucide-react"
 
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 type Props = {};
 
 const AuthButton = () => {
@@ -12,12 +47,12 @@ const AuthButton = () => {
 
   if (session) {
     return (
-      <div>
+      <div className="flex flex-col items-center">
         {session?.user?.name}
         <br />
         <button
           onClick={() => signOut()}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center"
         >
           Sign out
         </button>
@@ -25,12 +60,12 @@ const AuthButton = () => {
     );
   }
   return (
-    <div>
+    <div className="flex flex-col items-center">
       Not signed in
       <br />
       <button
         onClick={() => signIn()}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center"
       >
         Sign in
       </button>
@@ -40,12 +75,80 @@ const AuthButton = () => {
 
 const SideNavbar = (props: Props) => {
   return (
-    <div className="flex flex-col justify-between max-w-[200px] border-r border-r-gray-300 h-screen box-border p-4">
-      <div className="flex flex-col space-y-4">
-        <Link href={ROOT} className="font-medium text-xl">ShowerThoughts</Link>
-        <Link href={PROTECTED} className="hover:underline">Protected</Link>
+    <div className="flex flex-col max-w-[268px] justify-between border-r-gray-300 h-screen box-border border-r bg-muted/40">
+      <div className="hidden border-r bg-muted/40 md:block justify-between">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href={ROOT} className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">ShowerThoughts</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1 mt-2">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Home
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Book className="h-4 w-4" />
+                Entries
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              {/* <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Products{" "}
+              </Link> */}
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Friends
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Calendar className="h-4 w-4" />
+                Calendar
+              </Link>
+            </nav>
+          </div>
+          {/* <div className="mt-auto p-4">
+            <Card x-chunk="dashboard-02-chunk-0">
+              <CardHeader className="p-2 pt-0 md:p-4">
+                <CardTitle>Upgrade to Pro</CardTitle>
+                <CardDescription>
+                  Unlock all features and get unlimited access to our support
+                  team.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+                <Button size="sm" className="w-full">
+                  Upgrade
+                </Button>
+              </CardContent>
+            </Card>
+          </div> */}
+        </div>
+        <AuthButton />
       </div>
-      <AuthButton />
     </div>
   );
 };
