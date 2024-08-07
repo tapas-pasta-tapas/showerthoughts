3bb6e8d8681a9c36f611f6271b94de575f044351
Lee Zheng Jing <zhengjing.lee86@gmail.com>
2024-08-08 00:13:53 +0800

    Add Dockerfile

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { DEFAULT_REDIRECT } from "./lib/routes";
 import { PUBLIC_ROUTES } from "./lib/routes";
 
-export { default } from "next-auth/middleware"
+export async function middleware(request: NextRequest) {
+  const sessionCookie = request.cookies.get("next-auth.session-token");
+  const { nextUrl } = request;
 
-export const config = { matcher: ["/protected", "/entry"] }
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+
+  if (!sessionCookie && !isPublicRoute) {
+    return NextResponse.redirect(new URL(DEFAULT_REDIRECT, request.nextUrl));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
